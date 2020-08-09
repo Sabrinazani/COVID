239dc231e13cc041354ed21528e89d35eca8ac7b
Sabrinazani <67112714+Sabrinazani@users.noreply.github.com>
2020-08-09 15:30:55 -0300

    Update animação.py

--- animação.py
+++ animação.py
@@ -103,7 +103,7 @@ for a in range(len(reglist)):
         plt.figure(num=i)
         im.append(plt.figure(num=i))
     
-    imageio.mimsave("gif.gif",im) 
+    imageio.mimsave(relist[a]+".gif",im) 
     
     plt.close()
         
